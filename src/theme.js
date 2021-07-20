648fedb7bc9484cbedcb3e2bd3e81da646deada5
Iliya Lunev <lunevig90@gmail.com>
2021-07-20 18:01:21 +0300

    добавлен  localStorage

--- src/theme.js
+++ src/theme.js
@@ -10,7 +10,7 @@ const ref = {
 
 ref.themeSwitch.addEventListener('change', onChecked);
 
-function onChecked(e) {
+function onChecked() {
   const check = ref.themeSwitch.checked;
   if (check) {
     ref.body.classList.add(`dark-theme`);
@@ -19,4 +19,23 @@ function onChecked(e) {
     ref.body.classList.add(`light-theme`);
     ref.body.classList.remove(`dark-theme`);
   }
+}
+
+ref.themeSwitch.addEventListener('change', creatLocalStorage);
+
+function creatLocalStorage() {
+  const check = ref.themeSwitch.checked;
+  if (check) {
+    localStorage.setItem(`theme`, `dark-theme`);
+  } else {
+    localStorage.removeItem(`theme`);
+    localStorage.setItem(`theme`, `light-theme`);
+  }
+}
+
+const inLocal = localStorage.getItem(`theme`);
+
+if (inLocal === `dark-theme`) {
+  ref.body.classList.add(`dark-theme`);
+  ref.themeSwitch.checked = true;
 }
